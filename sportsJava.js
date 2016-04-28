4e8cee339958caeade3cf4ed132a3bcb8a828224
mimi_o <miong75@yahoo.com>
2016-04-28 01:58:23 +0000

    files

---
+++ sportsJava.js
@@ -0,0 +1,10 @@
+// JavaScript File
+
+
+$(document).ready(function(){
+    $("#list").click(function(){
+        $("#park").slideToggle("slow");
+        $("#aurora").slideToggle("slow");
+        $("#glacier").slideToggle("slow");
+    });
+});
